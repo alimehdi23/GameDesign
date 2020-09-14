2c95eecc4595807dcd274df1ba5550301830fddc
alimehdi23 <70219157+alimehdi23@users.noreply.github.com>
2020-09-14 10:50:10 -0500

    Add files via upload

--- basicprogramassignment.py
+++ basicprogramassignment.py
@@ -1,5 +1,5 @@
 x = 7
-y = 3
+y = 3.0
 a = "carrot"
 c = False
 print x + y
@@ -8,3 +8,7 @@ print x/y
 print x * y
 print y + 4
 print y > x
+print(type(x))
+print(type(a))
+print(type(y))
+print(type(c))
