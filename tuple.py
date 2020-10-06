c5de5175def0a60aac16f9707fd6e9ab4d4c4f08
alimehdi23 <70219157+alimehdi23@users.noreply.github.com>
2020-10-05 21:06:41 -0500

    Add files via upload

---
+++ tuple.py
@@ -0,0 +1,15 @@
+tuple= (21,36,14,25)
+tuple.count(25)
+coordinates = (4, 5)
+print(coordinates[0])
+#coordinates = (4, 5)=10 wont work
+coures = ['history','math','physics']
+print(coures)
+print(coures[2])
+print(coures[-2])
+print(coures[0:2])
+coures.append('art')
+coures.insert(0,'compscience')
+coures.pop()
+coures.reverse()
+coures.sort()
